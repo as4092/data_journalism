dc9a0e89923ff1580eed0d8d8a4ad5383e0a4aa7
as4092 <as4092@nyu.edu>
2014-10-22 22:50:33 -0400

    cleanedfolder

--- huffpo_files/js(4).php
+++
@@ -1,4 +0,0 @@
-/* From: production-mt-wfe-52-229-use1 : 1620 */
-window.UNB={config:{referrer:document.referrer,theme:"light",minimumBrowserWidth:320,devId:"sa1quMeQRBVzK6Jb"}};(function(){var b=document.createElement("script");b.id="unb_script";b.type="text/javascript";b.async=true;b.src="http://o.aolcdn.com/os/aol/unb.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(b,a)})();
-
-/* From: production-mt-wfe-52-229-use1 : 1620 *//* a6b99a76c969163c9163fd610764bbeccd02824f */
