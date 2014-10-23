14ee2094f2448bcb591fe923ff5ad16e3e0354be
as4092 <as4092@nyu.edu>
2014-10-22 22:48:35 -0400

    copy CSS

---
+++ huffpo_files/33877869_eff62fc45dff7948c5bf0a3bdc162ad8_creative_def.js
@@ -0,0 +1,119 @@
+(function() {
+  var creativeDefinition = {
+    customScriptUrl: '',
+    isDynamic: false,
+    delayedImpression: false,
+    standardEventIds: {
+      DISPLAY_TIMER: '2',
+      INTERACTION_TIMER: '3',
+      INTERACTIVE_IMPRESSION: '4',
+      FULL_SCREEN_VIDEO_PLAYS: '5',
+      FULL_SCREEN_VIDEO_COMPLETES: '6',
+      FULL_SCREEN_AVERAGE_VIEW_TIME: '7',
+      MANUAL_CLOSE: '8',
+      BACKUP_IMAGE_IMPRESSION: '9',
+      EXPAND_TIMER: '10',
+      VIDEO_PLAY: '11',
+      VIDEO_VIEW_TIMER: '12',
+      VIDEO_COMPLETE: '13',
+      VIDEO_INTERACTION: '14',
+      VIDEO_PAUSE: '15',
+      VIDEO_MUTE: '16',
+      VIDEO_REPLAY: '17',
+      VIDEO_MIDPOINT: '18',
+      FULL_SCREEN_VIDEO: '19',
+      VIDEO_STOP: '20',
+      VIDEO_FIRST_QUARTILE: '960584',
+      VIDEO_THIRD_QUARTILE: '960585',
+      VIDEO_UNMUTE: '149645',
+      FULL_SCREEN: '286263',
+      DYNAMIC_CREATIVE_IMPRESSION: '536393',
+      HTML5_CREATIVE_IMPRESSION: '871060'
+    },
+    exitEvents: [
+      {
+        name: 'Background_Exit',
+        reportingId: '2016545',
+        url: 'http://wecare.vfc.com/?utm_source\x3dDC-%esid!\x26utm_campaign\x3d%epid!\x26utm_medium\x3dads\x26utm_content\x3d%ecid!',
+        targetWindow: '_blank',
+        windowProperties: ''
+      },
+      {
+        name: 'Learn_More_Exit',
+        reportingId: '2016544',
+        url: 'http://wecare.vfc.com/?utm_source\x3dDC-%esid!\x26utm_campaign\x3d%epid!\x26utm_medium\x3dads\x26utm_content\x3d%ecid!',
+        targetWindow: '_blank',
+        windowProperties: ''
+      }
+    ],
+    timerEvents: [
+    ],
+    counterEvents: [
+    ],
+    childFiles: [
+      {
+        name: '728x90-v3.swf',
+        url: '/ads/richmedia/studio/pv2/33810377/20140917134040772/728x90-v3.swf',
+        isVideo: false
+      },
+      {
+        name: '728x90-worker-safety-backup.jpg',
+        url: '/ads/richmedia/studio/pv2/33810377/20140917134040772/728x90-worker-safety-backup.jpg',
+        isVideo: false
+      }
+    ],
+    videoFiles: [
+    ],
+    videoEntries: [
+    ],
+    primaryAssets: [
+      {
+        id: '33804734',
+        artworkType: 'FLASH',
+        displayType: 'BANNER',
+        width: '728',
+        height: '90',
+        servingPath: '/ads/richmedia/studio/pv2/33810377/20140917134040772/Main.swf',
+        zIndex: '1000000',
+        customCss: '',
+        flashArtworkTypeData: {
+          actionscriptVersion: '3',
+          wmode: 'opaque',
+          sdkVersion: '2.4.2'
+        },
+        htmlArtworkTypeData: null,
+        floatingDisplayTypeData: null,
+        expandingDisplayTypeData: null,
+        imageGalleryTypeData: null,
+        pageSettings:null,
+layoutsConfig: null,
+layoutsApi: null
+      }
+    ]
+  }
+  var rendererDisplayType = '';
+  rendererDisplayType += 'flash_';
+  var rendererFormat = 'inpage';
+  var rendererName = rendererDisplayType + rendererFormat;
+
+  var creativeId = '59531130';
+  var adId = '0';
+  var templateVersion = '200_53';
+  var studioObjects = window['studioV2'] = window['studioV2'] || {};
+  var creativeObjects = studioObjects['creatives'] = studioObjects['creatives'] || {};
+  var creativeKey = [creativeId, adId].join('_');
+  var creative = creativeObjects[creativeKey] = creativeObjects[creativeKey] || {};
+  creative['creativeDefinition'] = creativeDefinition;
+  var adResponses = creative['adResponses'] || [];
+  for (var i = 0; i < adResponses.length; i++) {
+    adResponses[i].creativeDto && adResponses[i].creativeDto.csiEvents &&
+        (adResponses[i].creativeDto.csiEvents['pe'] =
+            adResponses[i].creativeDto.csiEvents['pe'] || (+new Date));
+  }
+  var loadedLibraries = studioObjects['loadedLibraries'] = studioObjects['loadedLibraries'] || {};
+  var versionedLibrary = loadedLibraries[templateVersion] = loadedLibraries[templateVersion] || {};
+  var typedLibrary = versionedLibrary[rendererName] = versionedLibrary[rendererName] || {};
+  if (typedLibrary['bootstrap']) {
+    typedLibrary.bootstrap();
+  }
+})();
